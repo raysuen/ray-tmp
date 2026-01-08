c795984aa53e57c9eaa5c4ce6bc2bc606fc77361
raysuen <10438669+raysuen@user.noreply.gitee.com>
2026-01-08 12:44:30 +0800

    20260108

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-find . -name '.DS_Store' -o -name '._*' | grep -v '.git' | xargs rm -rf 2>/dev/null
+#find . -name '.DS_Store' -o -name '._*' | grep -v '.git' | xargs rm -rf 2>/dev/null
+find . \( -name '.DS_Store' -o -name '._*' \) -not -path '*/.git/*' -delete
 git add *
 git commit -m "`/Users/sunpeng/raysuen/bin/rdate.py -f "%Y%m%d"`"
 git push -u origin "master"
